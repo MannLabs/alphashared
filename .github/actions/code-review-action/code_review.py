52e8d5cbab9cfb0a548dfa79de9537bc06badbe2
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-08-08 17:15:01 +0200

    correct call

--- .github/actions/code-review-action/code_review.py
+++ .github/actions/code-review-action/code_review.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 # import requests
 # import subprocess
 from github import Github
@@ -7,36 +9,14 @@ g = Github(os.environ['GITHUB_TOKEN'])
 repo = g.get_repo(os.environ['GITHUB_REPOSITORY'])
 pr = repo.get_pull(int(os.environ['PR_NUMBER']))
 
-
 def get_file_content(file_path, commit_sha):
     try:
         return repo.get_contents(file_path, ref=commit_sha).decoded_content.decode()
     except:
         return ""  # Return empty string if file doesn't exist in that commit
 
-
 def get_file_patch(file):
     return file.patch if file.patch else ""
-
-
-changed_files = '${{ steps.changed-files.outputs.all_changed_files }}'.split()
-
-file_data = []
-for file in pr.get_files():
-    if file.filename in changed_files:
-        original_content = get_file_content(file.filename, pr.base.sha)
-        changed_content = get_file_content(file.filename, pr.head.sha)
-        patch = get_file_patch(file)
-        file_data.append({
-            "filename": file.filename,
-            "originalContent": original_content,
-            "changedContent": changed_content,
-            "patch": patch
-        })
-
-changed_files = '${{ steps.changed-files.outputs.all_changed_files }}'.split()
-print("XXX", changed_files)
-
 
 def format_code_review_data(data_list):
     formatted_string = "Beginning of Code Review Data:\n\n"
@@ -60,32 +40,46 @@ def format_code_review_data(data_list):
 
     return formatted_string
 
+changed_files = sys.argv[1].split()
+
+file_data = []
+for file in pr.get_files():
+    if file.filename in changed_files:
+        original_content = get_file_content(file.filename, pr.base.sha)
+        changed_content = get_file_content(file.filename, pr.head.sha)
+        patch = get_file_patch(file)
+        file_data.append({
+            "filename": file.filename,
+            "originalContent": original_content,
+            "changedContent": changed_content,
+            "patch": patch
+        })
 
 print("# ##############################################################################")
 print(format_code_review_data(file_data))
 print("# ##############################################################################")
 
-try:
-    review_comments = {file: [(1, 'cool')] for file in changed_files}
-    g = Github(os.environ['GITHUB_TOKEN'])
-    repo = g.get_repo(os.environ['GITHUB_REPOSITORY'])
-    pr = repo.get_pull(int(os.environ['PR_NUMBER']))
+# try:
+#     review_comments = {file: [(1, 'cool')] for file in changed_files}
+#     g = Github(os.environ['GITHUB_TOKEN'])
+#     repo = g.get_repo(os.environ['GITHUB_REPOSITORY'])
+#     pr = repo.get_pull(int(os.environ['PR_NUMBER']))
+#
+#     last_commit = list(pr.get_commits())[-1]
+#
+#     for filename, comments in review_comments.items():
+#         for line, comment in comments:
+#             print("XXX", line, comment)
+#             #pr.create_review_comment(body=comment, commit=last_commit, path=filename, line=int(line))
+#
+#     print('Code review completed and comments posted.')
+# except Exception as e:
+#     print(f'Error during code review: {str(e)}')
+#     import traceback
+#
+#     traceback.print_exc()
+#     exit(1)
 
-    last_commit = list(pr.get_commits())[-1]
-
-    for filename, comments in review_comments.items():
-        for line, comment in comments:
-            print("XXX", line, comment)
-            pr.create_review_comment(body=comment, commit=last_commit, path=filename, line=int(line))
-
-    print('Code review completed and comments posted.')
-except Exception as e:
-    print(f'Error during code review: {str(e)}')
-    import traceback
-
-    traceback.print_exc()
-    exit(1)
-shell: python
 #            response = requests.post(
 #                os.environ['REVIEW_API_URL'],
 #                json={"files": file_data},
