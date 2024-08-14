b790db4dbc4ddd8f3a79944db22036f186db6007
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-08-14 10:13:12 +0200

    dump only files

--- actions/get-code-review-input/files_to_string.py
+++ actions/get-code-review-input/files_to_string.py
@@ -16,13 +16,11 @@ def _concatenate_files(file_paths: list[str]) -> str:
     """Concatenate the content of multiple files into a single string."""
     files_contents = []
     for file_path in file_paths:
-        if os.path.exists(file_path):
+        if os.path.isfile(file_path):
             files_contents.append(f"START FILE '{file_path}' >>>>>>>>\n")
             with open(file_path, "r") as infile:
                 files_contents.append(infile.read())
             files_contents.append(f"\n<<<<<<<< END FILE '{file_path}'")
-        else:
-            print(f"Warning: File not found - {file_path}")
 
     return "\n\n".join(files_contents)
 
